3524d281592c91be6156af5729477186ea2314a6
ekaterinasugaj <shugay_k@ukr.net>
2022-02-20 18:36:57 +0200

    Test commit

--- Test.cs
+++ Test.cs
@@ -33,6 +33,7 @@ namespace Suicid_Xarakiri
     
         public void Test1(int a, int b, int c, int expected)
         {
+            Console.WriteLine("1242434r4324233243232323");
             int actual = EmptyClass.Testing(a, b, c);
             Assert.Equal(expected, actual);
         }
