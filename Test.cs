20c5542c98b1e9332e263c3da7a2c6ab61c611ed
ekaterinasugaj <shugay_k@ukr.net>
2022-02-20 18:31:43 +0200

    Final Test PR build

--- Test.cs
+++ Test.cs
@@ -33,7 +33,6 @@ namespace Suicid_Xarakiri
     
         public void Test1(int a, int b, int c, int expected)
         {
-            Console.WriteLine("1242434r43242343232323");
             int actual = EmptyClass.Testing(a, b, c);
             Assert.Equal(expected, actual);
         }
