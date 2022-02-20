e2e5ec70d352cc1d565fce8271871580620eebfc
ekaterinasugaj <shugay_k@ukr.net>
2022-02-20 16:05:16 +0200

    xxxxx

--- Test.cs
+++ Test.cs
@@ -7,7 +7,7 @@ namespace Suicid_Xarakiri
     public class Test
     {
         [Theory]
-        [InlineData(4, 5, 7, 8)]
+        [InlineData(3, 1, 2, 3)]
         [InlineData(3, 6, 1, 5)]
         [InlineData(6, 2, 4, 6)]
         [InlineData(15, 5, 2, 11)]
