13fb4760017f4f5df3f8b8ffe23aac46c2e7d90b
DikyBoom <59197258+DikyBoom@users.noreply.github.com>
2022-02-23 21:32:36 +0200

    Update EmptyClass.cs

--- EmptyClass.cs
+++ EmptyClass.cs
@@ -1,14 +1,2 @@
-﻿using System;
 
-
-namespace Suicid_Xarakiri
-{
-    public class EmptyClass
-    {
-        
-        public static int Testing(int a, int b, int c)
-        {
-            return (a + b + c) / 2;
-        }
-    }
 }
